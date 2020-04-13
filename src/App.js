e91e464a3c71ffd6f2e9df7fca76cdcd26a6f819
Carlos Baez <carlosbaez@Green-Planet.attlocal.net>
2020-04-12 22:40:39 -0400

    added the total component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Header from './components/Header'
+import Total from './components/Total';
 
 const App = props => {
   return (
@@ -8,7 +9,7 @@ const App = props => {
       <div className="box">
         <Header />
         <div className="box">
-
+          <Total />
         </div>
       </div>
     </div>
---
+++ src/components/Total.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+const Total = props => {
+    const price = useSelector(state => state.car.price)
+    const additionalPrice = useSelector(state => state.additionalPrice)
+
+    return (
+        <div className="content">
+            <h4>Total Amount: ${price + additionalPrice}</h4>
+        </div>
+    )
+}
+
+export default Total
